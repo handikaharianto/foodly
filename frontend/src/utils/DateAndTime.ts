d0dcbd61114bf90e990cde06aae44f32e83e7c46
Handika <handikaharianto01@gmail.com>
2023-06-11 01:57:36 +0800

    chore: add util functions for chat

--- frontend/src/utils/DateAndTime.ts
+++ frontend/src/utils/DateAndTime.ts
@@ -1,7 +1,23 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import calendar from "dayjs/plugin/calendar";
 
 export const getTimeFromNow = (time: string) => {
   dayjs.extend(relativeTime);
   return dayjs(time).fromNow();
+};
+
+export const setChatMessageTime = (time: string) => {
+  dayjs.extend(calendar);
+
+  const formattedTime = dayjs(time).calendar(null, {
+    sameDay: "h:mm A", // The same day ( 2:30 AM )
+    nextDay: "[Tomorrow at] h:mm A", // The next day ( Tomorrow at 2:30 AM )
+    nextWeek: "dddd [at] h:mm A", // The next week ( Sunday at 2:30 AM )
+    lastDay: "[Yesterday]", // The day before ( Yesterday )
+    lastWeek: "[Last] dddd", // Last week ( Last Monday )
+    sameElse: "DD/MM/YYYY", // Everything else ( 17/10/2011 )
+  });
+
+  return formattedTime;
 };
