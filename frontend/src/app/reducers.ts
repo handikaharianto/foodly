d6383888d1de7c6a54716ad21629c604aee9ace1
Handika <handikaharianto01@gmail.com>
2023-06-11 01:55:44 +0800

    feat: add communitySlice on frontend side

--- frontend/src/app/reducers.ts
+++ frontend/src/app/reducers.ts
@@ -3,11 +3,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import user from "../features/user/UserSlice";
 import communityApplication from "../features/communityApplication/CommunityApplicationSlice";
 import chat from "../features/chat/ChatSlice";
+import community from "../features/community/communitySlice";
 
 const rootReducer = combineReducers({
   user,
   communityApplication,
   chat,
+  community,
 });
 
 export default rootReducer;
---
+++ frontend/src/features/community/communitySlice.ts
@@ -0,0 +1,104 @@
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
+import { Community, UpdateCommunity, NewCommunity } from "./types";
+import { executeAsyncThunk, privateAxios } from "../../api/axios";
+import { PaginatedData } from "../../utils/types";
+import { RootState } from "../../app/store";
+
+export const createCommunity = executeAsyncThunk<NewCommunity, void>(
+  "community/createCommunity",
+  (req) => privateAxios.post("/communities", req)
+);
+
+export const getAllCommunities = executeAsyncThunk<
+  { limit: number; page: number },
+  PaginatedData<Community>
+>("community/getAllCommunities", (req) =>
+  privateAxios.post("/communities/list", req)
+);
+
+export const getOneCommunity = executeAsyncThunk<
+  { communityId: string },
+  Community
+>("community/getOneCommunity", (req) =>
+  privateAxios.get(`/communities/${req.communityId}`)
+);
+
+export const updateOneCommunity = executeAsyncThunk<UpdateCommunity, Community>(
+  "community/updateOneCommunity",
+  (req) =>
+    privateAxios.put(`/communities/${req.communityId}`, {
+      name: req.name,
+      description: req.description,
+    })
+);
+
+type CommunityState = {
+  community: Community | null;
+  communityList: Community[];
+  currentPage: number;
+  totalPages: number;
+  totalData: number;
+  isLoading: boolean;
+};
+
+const initialState: CommunityState = {
+  community: null,
+  communityList: [],
+  currentPage: 0,
+  totalPages: 0,
+  totalData: 0,
+  isLoading: false,
+};
+
+const communitySlice = createSlice({
+  name: "community",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createCommunity.fulfilled, (state, action) => {
+        state.isLoading = false;
+      })
+      .addCase(getAllCommunities.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.communityList = action.payload.data;
+        state.currentPage = action.payload.currentPage;
+        state.totalPages = action.payload.totalPages;
+        state.totalData = action.payload.totalData;
+      })
+      .addCase(getOneCommunity.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.community = action.payload;
+      })
+      .addCase(updateOneCommunity.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.community = action.payload;
+      })
+      .addMatcher(
+        isAnyOf(
+          createCommunity.pending,
+          getAllCommunities.pending,
+          getOneCommunity.pending,
+          updateOneCommunity.pending
+        ),
+        (state, action) => {
+          state.isLoading = true;
+        }
+      )
+      .addMatcher(
+        isAnyOf(
+          createCommunity.rejected,
+          getAllCommunities.rejected,
+          getOneCommunity.rejected,
+          updateOneCommunity.rejected
+        ),
+        (state, action) => {
+          state.isLoading = true;
+        }
+      );
+  },
+});
+
+export const communityState = (state: RootState) => state.community;
+
+export default communitySlice.reducer;
