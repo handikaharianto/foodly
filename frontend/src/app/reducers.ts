1d0c13964b599794abb9cc62c29e4bda1e33788f
Handika <handikaharianto01@gmail.com>
2023-07-03 14:36:46 +0800

    feat: add donation slice

--- frontend/src/app/reducers.ts
+++ frontend/src/app/reducers.ts
@@ -4,12 +4,14 @@ import user from "../features/user/UserSlice";
 import communityApplication from "../features/communityApplication/CommunityApplicationSlice";
 import chat from "../features/chat/ChatSlice";
 import community from "../features/community/communitySlice";
+import donation from "../features/donation/donationSlice";
 
 const rootReducer = combineReducers({
   user,
   communityApplication,
   chat,
   community,
+  donation,
 });
 
 export default rootReducer;
