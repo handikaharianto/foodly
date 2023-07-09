84b0166f865e2bbbe3dbad53944249eacba013ea
Handika <handikaharianto01@gmail.com>
2023-07-09 18:00:46 +0800

    chore: pass user id through socket.io when logging out

--- frontend/src/components/common/SetupAxiosInterceptor.tsx
+++ frontend/src/components/common/SetupAxiosInterceptor.tsx
@@ -3,20 +3,24 @@ import { privateAxios, publicAxios } from "../../api/axios";
 import { LoginUserResponse } from "../../features/user/types";
 import { useNavigate } from "react-router-dom";
 import { USER_OFFLINE, socket } from "../../socket/socket";
+import { useAppSelector } from "../../app/hooks";
+import { userState } from "../../features/user/UserSlice";
 
 const SetupAxiosInterceptor = ({ children }: { children: JSX.Element }) => {
   const [isSet, setIsSet] = useState(false);
+
+  const { loggedInUser } = useAppSelector(userState);
 
   const navigate = useNavigate();
 
   useEffect(() => {
     const logoutUser = () => {
+      socket.emit(USER_OFFLINE, loggedInUser?._id);
+      socket.disconnect();
+
       delete privateAxios.defaults.headers.common.Authorization;
       window.localStorage.clear();
       navigate("/sign-in", { replace: true });
-
-      socket.emit(USER_OFFLINE);
-      socket.disconnect();
     };
 
     // TODO: backend returns invalid access token although the access token is not expired yet.
