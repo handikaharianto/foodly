c6fb3822dab1a0aacf8819f8b9a338d6e19a73f4
Handika <handikaharianto01@gmail.com>
2023-07-14 22:36:30 +0800

    chore: add util function to calculate distance

--- frontend/src/utils/community.ts
+++ frontend/src/utils/community.ts
@@ -1,3 +1,5 @@
+import * as turf from "@turf/turf";
+
 import { CommunityAddress } from "../features/community/types";
 
 export const formatCommunityAddress = (address: CommunityAddress): string => {
@@ -6,4 +8,22 @@ export const formatCommunityAddress = (address: CommunityAddress): string => {
   return address.addressLine2
     ? `${address.addressLine2}, ${communityAddress}`
     : communityAddress;
+};
+
+export type CoordinateType = [number, number];
+type TurfOptionsType = { units: "kilometers" | "miles" };
+
+export const calculateDistance = (
+  from: CoordinateType,
+  to: CoordinateType,
+  options: TurfOptionsType
+) => {
+  if (from === null || !to[0] || !to[1]) {
+    return;
+  }
+
+  const fromCoordinate = turf.point(from);
+  const toCoordinate = turf.point(to);
+
+  return turf.distance(fromCoordinate, toCoordinate, options).toFixed(1);
 };
